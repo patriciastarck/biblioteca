113e378d5d492e297e877952c1cbbee090c366fc
Patricia <patriciastarck@gmail.com>
2024-01-14 09:22:13 -0300

    feat: Adicionei formulário HTML

--- biblio/src/main/java/com/psb/biblio/controller/UsuarioController.java
+++ biblio/src/main/java/com/psb/biblio/controller/UsuarioController.java
@@ -1,21 +1,40 @@
 package com.psb.biblio.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.psb.biblio.model.Usuario;
+import com.psb.biblio.repository.UsuarioRepository;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
-
-    @GetMapping("/novo")
-    public String adicionarUsuario(Model model) {
-        model.addAttribute("usuario", new Usuario());
-        return "/publica-criar-usuario";
-        
-    }
-    
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@GetMapping("/novo")
+	public String adicionarUsuario(Model model) {
+		model.addAttribute("usuario", new Usuario());
+		return "/publica-criar-usuario";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvarUsuario(@Valid Usuario usuario, BindingResult result, 
+				RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			return "/publica-criar-usuario";
+		}	
+		usuarioRepository.save(usuario);
+		attributes.addFlashAttribute("mensagem", "Usuário salvo com sucesso!");
+		return "redirect:/usuario/novo";
+	}
 }
